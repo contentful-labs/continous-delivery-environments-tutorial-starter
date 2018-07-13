4d14b65f04fd4b30ac4b9580b5b03d8b0ab0f19b
Shy Ruparel <Shyamalruparel1991@gmail.com>
2018-07-13 11:08:51 -0400

    Removed completed steps for tutorial

--- migrations/0_0_5.js
+++
@@ -1,9 +0,0 @@
-module.exports = function runMigration(migration) {
-  const post = migration.editContentType("post");
-  post
-    .createField("last_appearance")
-    .name("last_appearance")
-    .type("Symbol")
-    .required(false);
-  return;
-};
